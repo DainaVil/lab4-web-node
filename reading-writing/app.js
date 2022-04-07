548065ec29922dec047044db98ef8c4a46d3b74f
Daina Viliunaite <daina06vil@yandex.ru>
2022-04-07 21:11:00 +0300

    task 1.2 complete

--- reading-writing/app.js
+++ reading-writing/app.js
@@ -7,36 +7,35 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.get('/products', (req, res) => {
-  res.send(products);
-})
+app.route('/products')
+	.get((req, res) => {
+		res.json(products);
+    })
 
-app.post('/products', function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-  
-  let data = new Object(req.body);
-  data['id'] = products.length + 1;
-  products.push(data);
-  res.send(products);
-});
+	.post((req, res) => {
+    	const newProduct = { ...req.body, id: products.length + 1 }
+    	products = [...products, newProduct]
+    	res.json(newProduct);
+   	})
 
-app.put('/products', function (req, res) {
-    products.forEach(product => {
-    if (product.id === +req.body['id']) {
-      product.name = req.body['name']
-    }
-  })
-  res.send(products)
-});
+  	.put((req, res) => {
+    	let updatedProduct;
+		products = products.map(p => {
+			if (p.id === req.body.id) {
+				updatedProduct = { ...p, ...req.body };
+        		return updatedProduct;
+			}
+			return p; 
+		})
+    	res.json(updatedProduct);
+   	})
 
-app.delete('/products/:id', function (req, res) {
-	for (var i = 0; i < products.length; i++) {
-		const productToDelete = products.find(p => p.id === +req.body.id);
-      	products = products.filter(p => p.id !== +req.body.id);
-      	res.send(productToDelete);
-    	
-  	}
-});
+    .delete((req, res) => {
+    	const deletedProduct = products.find(p => p.id === +req.body.id);
+    	products = products.filter(p => p.id !== +req.body.id);
+    	res.json(deletedProduct);
+	})
+
 
 app.listen(port, () => console.log(`Прослушивание на порту ${port}!`))
   
