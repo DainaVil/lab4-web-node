7a8459724e65f432f8e7c8df65e1f60b7ce86505
Daina Viliunaite <daina06vil@yandex.ru>
2022-04-07 20:11:17 +0300

    task 1.1 complete

--- reading-writing/app.js
+++ reading-writing/app.js
@@ -8,19 +8,34 @@ app.use(bodyParser.json());
 let products = [];
 
 app.get('/products', (req, res) => {
-  // *РЕАЛИЗОВАТЬ*
+  res.send(products);
 })
 
 app.post('/products', function (req, res) {
-  // *РЕАЛИЗОВАТЬ*
+  if (!req.body) return res.sendStatus(400);
+  
+  let data = new Object(req.body);
+  data['id'] = products.length + 1;
+  products.push(data);
+  res.send(products);
 });
 
 app.put('/products', function (req, res) {
-  // *РЕАЛИЗОВАТЬ*
+    products.forEach(product => {
+    if (product.id === +req.body['id']) {
+      product.name = req.body['name']
+    }
+  })
+  res.send(products)
 });
 
 app.delete('/products/:id', function (req, res) {
-  // *РЕАЛИЗОВАТЬ*
+	for (var i = 0; i < products.length; i++) {
+    	if (products[i].id === +req.body['id']) {
+    		res.send(products[i])
+      		products.splice(i, 1);
+    	}
+  	}
 });
 
 app.listen(port, () => console.log(`Прослушивание на порту ${port}!`))
